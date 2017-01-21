47d5923da71a7ccee82c221ecb00c3b8c5fcd884
crintus <johandp92@gmail.com>
2017-01-21 17:56:33 +0200

    Added initial models

--- src/paparazzi/models.py
+++ src/paparazzi/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+import os
+import uuid
+import time
 
-# Create your models here.
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+from administration.models import User
+
+
+def get_unique_filename(filename):
+    name, ext = os.path.splitext(filename)
+    return "{}_{}{}".format(uuid.uuid4().hex, str(int(time.time())), ext)
+
+
+def get_image_path(instance, filename):
+    return os.path.join('users', str(instance.user.id), 'images', get_unique_filename(filename))
+
+
+class CreatedAt(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Photo(CreatedAt):
+    user = models.ForeignKey(User)
+    image = models.ImageField(upload_to=get_image_path, null=True, blank=True)
+    data = JSONField(default={}, null=True, blank=True)
+    is_profile = models.BooleanField(default=False)
