d7be27cf3e662a82d32053b5a1dae1c870da4a8e
crintus <johandp92@gmail.com>
2017-01-21 18:45:10 +0200

    - Set `TimeStampedModel` to abstract - Added `__str__` value for `Photo`

--- src/paparazzi/models.py
+++ src/paparazzi/models.py
@@ -17,13 +17,19 @@ def get_image_path(instance, filename):
     return os.path.join('users', str(instance.user.id), 'images', get_unique_filename(filename))
 
 
-class CreatedAt(models.Model):
+class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
 
-class Photo(CreatedAt):
+
+class Photo(TimeStampedModel):
     user = models.ForeignKey(User)
     image = models.ImageField(upload_to=get_image_path, null=True, blank=True)
     data = JSONField(default={}, null=True, blank=True)
     is_profile = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.image.get_directory_name()
